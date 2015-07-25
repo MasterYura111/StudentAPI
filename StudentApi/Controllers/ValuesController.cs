e27a883c605d02118eed4b5ec12e330f24d22abf
MasterYura111 <masteryura2012@gmail.com>
2015-07-25 14:59:23 +0300

    first commit

---
+++ StudentApi/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentApi.Models;
+
+namespace StudentApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        StudentContext db = new StudentContext();
+
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return db.Students;
+        }
+
+        public Student GetStudent(int id)
+        {
+            Student student = db.Students.Find(id);
+
+            return student;
+
+        }
+
+
+        [HttpPost]
+        public void CreateStudent([FromBody]Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+        }
+
+
+        [HttpPut]
+        public void EditStudent(int id, [FromBody]Student student)
+        {
+            if (id == student.Id)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+
+            }
+        }
+
+        [HttpDelete]
+        public void DeleteStudent(int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student != null)
+            {
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
